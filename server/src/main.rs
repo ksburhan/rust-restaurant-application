679003b6fa86e96874cbf62dc0dc66181133c67e
ksburhan <burhan.koeseler@hotmail.de>
2022-05-24 17:37:29 +0200

    initial rocket setup for webserver

---
+++ server/src/main.rs
@@ -0,0 +1,14 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
